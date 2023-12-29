7a02d97d843a8e907ac792826384fb6396a2f259
manish01101 <manishchotu4@gmail.com>
2023-12-30 00:09:58 +0530

    hooks and methods in mongoose

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import AuthRoles from '../utils/authRoles'
-
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema(
   {
@@ -29,5 +29,20 @@ const userSchema = new mongoose.Schema(
   },
   {timestamps: true}
 )
+
+//encrypt the password before saving
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next()
+  this.password = await bcrypt.hash(this.password, 10)
+  next();
+})
+
+userSchema.methods = {
+  //compare password
+  comparePassword: async function(enterPassword) {
+    return await bcrypt.compare(enterPassword, this.password)
+  }
+}
+
 
 export default mongoose.model("User", userSchema)
