c27a6faad90ee41870ce46ebedec8763d5670a46
manish01101 <manishchotu4@gmail.com>
2023-12-30 01:59:46 +0530

    forgot password functionality

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 const config = {
   PORT: process.env.PORT || 3000,
-  MONOGODB_URL: process.env.MONOGODB_URL || "mongodb://localhost:27017/ecom"
+  MONOGODB_URL: process.env.MONOGODB_URL || "mongodb://localhost:27017/ecom",
+  JWT_SECRET: process.env.JWT_SECRET || "yoursecret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "15d"
 }
 
 export default config
