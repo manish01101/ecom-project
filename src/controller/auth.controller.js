02b54fffc884012569b95691f3a3ba9766724f15
manish01101 <manishchotu4@gmail.com>
2023-12-31 01:42:46 +0530

    user logged in and out

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -46,3 +46,43 @@ export const signUp = asyncHandler(async(req, res) => {
   })
 
 })
+
+export const login = asyncHandler(async(req, res) => {
+  const {email, password} = req.body
+
+  // validation
+  if(!email || !password) {
+    throw new CustomError("Please fill all details", 400)
+  }
+
+  const user = User.findOne({email}).select("+password")
+  if(!user) {
+    throw new CustomError("Invalid credentiials", 400)
+  }
+
+  const isPasswordMatched = await user.comparePassword(password)
+
+  if(isPasswordMatched) {
+    const token = user.getJWTtoken()
+    user.password = undefined
+    res.cookie("token", token, cookieOptions)
+    return res.status(200).json({
+      success: true,
+      token,
+      user
+    })
+  }
+  throw new CustomError("Password is incorrect", 400)
+
+})
+
+export const logout = asyncHandler(async(req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true //this shows server-side cookie
+  })
+  res.status(200).json({
+    success: true,
+    message: "Logged OUt"
+  })
+})
